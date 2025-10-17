38716c1cd3638601d070faf2952babe2bcff6a5d
shiva nirapure <shivanirapure2003@gmail.com>
2025-10-17 22:05:03 +0530

    adding assignment docs

--- loans/serializers.py
+++ loans/serializers.py
@@ -10,3 +10,57 @@ class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
         fields = '__all__'  # include all fields of Loan
+class CustomerRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'first_name', 'last_name', 'phone', 'monthly_income', 'approved_limit', 'age']
+
+    # Override create to calculate approved_limit
+    def create(self, validated_data):
+        monthly_salary = validated_data['monthly_income']
+        approved_limit = round(monthly_salary * 36 / 100000) * 100000  # nearest lakh
+        validated_data['approved_limit'] = approved_limit
+        return super().create(validated_data)
+
+
+class LoanEligibilitySerializer(serializers.Serializer):
+    customer_id = serializers.IntegerField()
+    loan_amount = serializers.FloatField()
+    interest_rate = serializers.FloatField()
+    tenure = serializers.IntegerField()
+
+class LoanEligibilityResponseSerializer(serializers.Serializer):
+    customer_id = serializers.IntegerField()
+    approval = serializers.BooleanField()
+    interest_rate = serializers.FloatField()
+    corrected_interest_rate = serializers.FloatField()
+    tenure = serializers.IntegerField()
+    monthly_installment = serializers.FloatField()
+
+
+class LoanCreateSerializer(serializers.Serializer):
+    customer_id = serializers.IntegerField()
+    loan_amount = serializers.FloatField()
+    interest_rate = serializers.FloatField()
+    tenure = serializers.IntegerField()
+
+class LoanCreateResponseSerializer(serializers.Serializer):
+    loan_id = serializers.IntegerField(allow_null=True)
+    customer_id = serializers.IntegerField()
+    loan_approved = serializers.BooleanField()
+    message = serializers.CharField()
+    monthly_installment = serializers.FloatField()
+
+
+
+class LoanDetailCustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'first_name', 'last_name', 'phone', 'age']
+
+class LoanDetailSerializer(serializers.ModelSerializer):
+    customer = LoanDetailCustomerSerializer(read_only=True)
+
+    class Meta:
+        model = Loan
+        fields = ['id', 'customer', 'amount', 'interest_rate', 'monthly_installment', 'term_months']
